29f57440c87870447aee452723d418258d965994
Andrew <andrew.ghastine@gmail.com>
2024-10-13 15:40:12 -0400

    main method

--- src/PatientPanel.java
+++ src/PatientPanel.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  *
  * @author A. Ghastine
  */
-public final class ProofOfConcept {
+public final class PatientPanel {
 
     /**
      * Private class of Patient with name, PCP, and dates fields.
@@ -64,6 +64,7 @@ public final class ProofOfConcept {
         public Patient(String n, String p) {
             this.name = n;
             this.pcp = p;
+            this.dates = new ArrayList<>();
         }
 
         /**
@@ -107,7 +108,7 @@ public final class ProofOfConcept {
     /**
      * No-argument constructor.
      */
-    public ProofOfConcept() {
+    public PatientPanel() {
         this.patients = new ArrayList<Patient>();
     }
 
@@ -126,13 +127,11 @@ public final class ProofOfConcept {
      *
      * @param name
      * @param pcp
-     * @param dates
      * @requires [patient is in @code this]
      * @return the removed patient.
      *
      */
-    public Patient removePatient(String name, String pcp,
-            ArrayList<String> dates) {
+    public Patient removePatient(String name, String pcp) {
         return this.patients
                 .remove(this.patients.lastIndexOf(new Patient(name, pcp)));
     }
@@ -157,4 +156,26 @@ public final class ProofOfConcept {
                 .get(this.patients.lastIndexOf(new Patient(name, pcp))).dates;
     }
 
+    /**
+     * Main method.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        PatientPanel practice = new PatientPanel();
+        System.out.println(practice.toString());
+        System.out.println(practice.size());
+        practice.addPatient("Harry", "Dr. Peck");
+        ArrayList<String> visits = practice.getVisits("Harry", "Dr. Peck");
+        visits.add("01/15/2024");
+        visits.add("04/15/2024");
+        System.out.println(practice.toString());
+        System.out.println(practice.size());
+        Patient patient = practice.removePatient("Harry", "Dr. Peck");
+        System.out.println(patient.name());
+        System.out.println(patient.pcp());
+        System.out.println(patient.dates());
+        System.out.println(practice.toString());
+        System.out.println(practice.size());
+    }
 }
